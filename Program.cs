8f9fab8951a74699ce8503b7be818f65546517c9
David Young <thedewi@users.noreply.github.com>
2023-06-21 23:33:42 +1000

    Add command execution (non-query)

--- Program.cs
+++ Program.cs
@@ -5,7 +5,14 @@ if (args.Length == 0) {
     return;
 }
 
+if (args.Length == 1)
+{
+    Console.Error.WriteLine("Please supply a query.");
+    return;
+}
+
 using var db = new SqlConnection(args[0]);
-using var cmd = new SqlCommand("select 'Connected.'", db);
+using var cmd = new SqlCommand(args[1], db);
 db.Open();
-Console.WriteLine(cmd.ExecuteScalar());
+var rowsAffected = cmd.ExecuteNonQuery();
+Console.WriteLine($"Query succeeded. Affected rows: {rowsAffected}");
